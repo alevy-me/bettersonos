e1b9a4c6aa30d36998125498faadc53083338771
alevy-me <a@alevy.me>
2025-06-06 11:33:15 -0500

    Added Copyright/License to BetterSonosApp.swift

--- BetterSonos/BetterSonosApp.swift
+++ BetterSonos/BetterSonosApp.swift
@@ -1,8 +1,20 @@
+// BetterSonos: A native SwiftUI client for the node-sonos-http-api.
+// Copyright (C) 2025 Andrew Levy
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 //  BetterSonosApp.swift
-//  BetterSonos
-//
-//  Created by Andrew Levy on 4/18/25. Updated for multi-network 5/20/25
 //
 
 import SwiftUI
